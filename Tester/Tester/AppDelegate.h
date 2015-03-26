30a0d4766648c5afd02c6ea27813b7709acb6d84
sarah-abdelrazak <sarah.abdelrazak@thirdwayv.com>
2015-03-26 13:54:15 +0200

    Tester
    Signed-off-by: sarah-abdelrazak <sarah.abdelrazak@thirdwayv.com>

---
+++ Tester/Tester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tester
+//
+//  Created by Sarah Abdelrazak on 3/26/15.
+//  Copyright (c) 2015 sarah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tester/Tester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tester
+//
+//  Created by Sarah Abdelrazak on 3/26/15.
+//  Copyright (c) 2015 sarah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
